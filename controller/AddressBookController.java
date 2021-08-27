2a193482b8829fb9f550f412b9ab2c480298a4c3
Daminichandrakar <damini.2201@gmail.com>
2021-08-27 13:34:09 +0530

    Welcome messgage

---
+++ controller/AddressBookController.java
@@ -0,0 +1,7 @@
+package com.bl.controller;
+
+public class AddressBookController {
+       public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook");
+	}
+}
---
+++ service/AddressBookService.java
@@ -0,0 +1,5 @@
+package com.bl.service;
+
+public class AddressBookService {
+
+}
